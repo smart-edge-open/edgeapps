a7b22860abf5ff60d985eeb3391c2e292e1e9be4
damiankopyto <48013534+damiankopyto@users.noreply.github.com>
2019-11-22 11:37:11 +0000

    Reversing to DPDK-18.08 and Flexran Patch for support for BBDEV 5G (#15)

--- build/fpga-sample-app/build-image.sh
+++ build/fpga-sample-app/build-image.sh
@@ -15,6 +15,19 @@
 # limitations under the License.
 #########################################################
 
+BBDEV_PATCH=./flexran-dpdk-bbdev-v19-10.patch
+
+#Check for existance of BBDEV v19.10 patch for DPDK 18.08
+if test -f "$BBDEV_PATCH"; then
+    echo "BBDEV patch found"
+else
+    echo "$BBDEV_PATCH does not exist." \
+	   "Please place the BBDEV patch for DPDK 18.08 into root directory of this script.\n" \
+	   "The patch is provided as part of v19.10 Release of FlexRAN package.\n" \
+	   "The script will now exit, the image will not be created.\n"
+    exit
+fi
+
 sudo docker build \
     --build-arg http_proxy=$http_proxy \
     --build-arg https_proxy=$https_proxy \
