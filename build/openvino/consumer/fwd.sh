4bc2b5da160fa2e900d4dacd679978da8865c624
Amr Mokhtar <amr.mokhtar@intel.com>
2019-12-09 10:34:58 +0000

    force pkt_size to 1200B for video streaming

--- build/openvino/consumer/fwd.sh
+++ build/openvino/consumer/fwd.sh
@@ -19,6 +19,6 @@ while :
 do
   taskset -c 1 ffmpeg -re -async 1 -vsync -1 -f mjpeg -r 30 -i vidfifo.mjpeg \
     -vcodec mjpeg -b:v 50M -s 1280x720 -c:v copy \
-    -f rtp rtp://analytics.openness:5001 > \
+    -pkt_size 1200 -f rtp rtp://analytics.openness:5001 > \
     /dev/null 2>&1 < /dev/null
 done
