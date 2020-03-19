e5977de30a724e314d7f8164963b710b148a1447
xiaopeng,tong <xiaopeng.tong@intel.com>
2020-03-19 17:47:08 +0800

    change analytics.openness host entry to align with app-on-boarding document

--- applications/openvino/consumer/start.sh
+++ applications/openvino/consumer/start.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 2019 Intel Corporation
 
 source /opt/intel/openvino/bin/setupvars.sh
-echo "192.168.200.10 analytics.openness" >> /etc/hosts
+echo "192.168.1.10 analytics.openness" >> /etc/hosts
 ./fwd.sh &
 go run main.go openvino.go eaa_interface.go
 fg
