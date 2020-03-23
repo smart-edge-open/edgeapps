dce6aa09f551767143c6943d4304d737b676063e
damiankopyto <48013534+damiankopyto@users.noreply.github.com>
2020-03-23 17:36:32 +0000

    Fix - allowing for RT and non-Rt kernel header download (#51)
    * Fix - allowing for RT and non-Rt kernel header download
    
    * Updating DPDK to 19.11 due to issues with 18.08 and VFIO
    Updating DPDK 19.11 FlexRAN patch name

--- applications/fpga-sample-app/build-image.sh
+++ applications/fpga-sample-app/build-image.sh
@@ -3,15 +3,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019 Intel Corporation
 
-BBDEV_PATCH=./flexran-dpdk-bbdev-v19-10.patch
+BBDEV_PATCH=./dpdk_19.11_new.patch
 
-#Check for existance of BBDEV v19.10 patch for DPDK 18.08
+##Check for existance of BBDEV v19.10 patch for DPDK 18.08
 if test -f "$BBDEV_PATCH"; then
     echo "BBDEV patch found"
 else
     echo "$BBDEV_PATCH does not exist." \
-	   "Please place the BBDEV patch for DPDK 18.08 into root directory of this script.\n" \
-	   "The patch is provided as part of v19.10 Release of FlexRAN package.\n" \
+	   "Please place the BBDEV patch for DPDK 19.11 into root directory of this script.\n" \
+	   "The patch is provided as part of v19.12 Release of FlexRAN package.\n" \
 	   "The script will now exit, the image will not be created.\n"
     exit
 fi
