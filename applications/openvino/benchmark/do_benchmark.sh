0e084ce1121a643cd1874c5126ad81b494560aab
Ma Changping <changping.ma@intel.com>
2020-06-05 14:17:59 +0800

    update openvino version to 2020.2.120 both benchmark and consumer app.
    Signed-off-by: Ma Changping <changping.ma@intel.com>

--- applications/openvino/benchmark/do_benchmark.sh
+++ applications/openvino/benchmark/do_benchmark.sh
@@ -7,5 +7,5 @@ source /opt/intel/openvino/bin/setupvars.sh
 cd /opt/intel/openvino/deployment_tools/demo
 ./demo_squeezenet_download_convert_run.sh -d HDDL
 
-/root/inference_engine_samples_build/intel64/Release/benchmark_app -i /opt/intel/openvino/deployment_tools/demo/car.png -m /root/openvino_models/ir/FP16//classification/squeezenet/1.1/caffe/squeezenet1.1.xml -d HDDL -nireq 32 -niter 9999999 -api async
+/root/inference_engine_samples_build/intel64/Release/benchmark_app -i /opt/intel/openvino/deployment_tools/demo/car.png -m /root/openvino_models/ir/public/squeezenet1.1/FP16/squeezenet1.1.xml -d HDDL -nireq 32 -niter 9999999 -api async
 
