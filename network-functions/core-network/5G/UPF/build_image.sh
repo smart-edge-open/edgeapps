56e7ff18b6aab6ad6675301a5fa9d1024439369c
Sudhir Pola <sudhir.pola@intel.com>
2020-06-10 22:21:47 +0530

    Added the upf configuration and start script, docker image script and updated docker image to copy the upf run script

--- network-functions/core-network/5G/UPF/build_image.sh
+++ network-functions/core-network/5G/UPF/build_image.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2020 Intel Corporation
 
+FROM ubuntu:18.04
 FROM ubuntu:18.04
 
 ENV http_proxy=$http_proxy
@@ -8,11 +11,10 @@ ENV https_proxy=$https_proxy
 ENV no_proxy=$no_proxy
 
 RUN apt-get update && \
-    apt-get -y install iputils-ping ethtool && \
+    apt-get -y install vim gdb iputils-ping ethtool && \
     apt-get -y install python-pip libcap-ng-dev gawk pciutils kmod && \
     apt-get -y install libc6 pciutils python3 lsb-base libnuma-dev iproute2 findutils apt-utils  libtool fakeroot libmbedtls-dev libhyperscan4 sudo
 
 WORKDIR /root
 
-COPY run_upf.sh ./run_upf.sh
-COPY 5g_upf_vpp ./5g_upf_vpp
+
