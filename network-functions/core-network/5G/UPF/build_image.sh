89bb329cd8d443b129a9069258639fa47cd1cb4e
Sudhir Pola <sudhir.pola@intel.com>
2020-06-10 22:37:50 +0530

    fixes in dockerfile, image file and updated documentation for the charts file

--- network-functions/core-network/5G/UPF/build_image.sh
+++ network-functions/core-network/5G/UPF/build_image.sh
@@ -3,18 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2020 Intel Corporation
 
-FROM ubuntu:18.04
-FROM ubuntu:18.04
-
-ENV http_proxy=$http_proxy
-ENV https_proxy=$https_proxy
-ENV no_proxy=$no_proxy
-
-RUN apt-get update && \
-    apt-get -y install vim gdb iputils-ping ethtool && \
-    apt-get -y install python-pip libcap-ng-dev gawk pciutils kmod && \
-    apt-get -y install libc6 pciutils python3 lsb-base libnuma-dev iproute2 findutils apt-utils  libtool fakeroot libmbedtls-dev libhyperscan4 sudo
-
-WORKDIR /root
+sudo docker build \
+    --build-arg http_proxy=$http_proxy \
+    --build-arg https_proxy=$https_proxy \
+    --build-arg no_proxy=$no_proxy \
+    -t upf-cnf:1.0 .
 
 
