dd46f438687be01d4afceef25a0ee26823aa0c21
xiaopeng,tong <xiaopeng.tong@intel.com>
2020-06-16 23:52:11 +0800

    add docker file and k8s yaml for vas sample app

--- applications/vas-sample-app/cmd/application.go
+++ applications/vas-sample-app/cmd/application.go
@@ -33,20 +33,12 @@ type VASInstanceStatus struct {
 	State       string  `json:"state"`
 }
 
-
-func main () {
-
-   // Interface: GET /pipelines for Return supported pipelines
-   log.Println("GET /pipelines for Return supported pipelines")
-   resp, err := grequests.Get("http://localhost:8080/pipelines", nil)
-   if err != nil {
-     log.Fatalln ("Get Failed with: ", err)
-   }
-   log.Println(resp.String())
-   log.Println("Completed Get Pipelines .......")
+func postVAServingRequest(vaEndPoint string, vaPipeline string) error {
 
    // Interface: POST /pipelines/{name}/{version} for Start new pipeline instance
-   log.Println("Starting POST Request .......")
+   var endPointStr string 
+   endPointStr = vaEndPoint+"/pipelines/"+vaPipeline
+   log.Println("Starting POST Request:"+endPointStr)
    var src Source
    src.URI = "https://github.com/intel-iot-devkit/sample-videos/blob/master/bottle-detection.mp4?raw=true"
    src.Type = "uri"
@@ -58,20 +50,24 @@ func main () {
    var vasPostJson, _ = json.Marshal(vasPost)
    var vasRequestOption = &grequests.RequestOptions{}
    vasRequestOption.JSON = string(vasPostJson)
-   resp, err = grequests.Post("http://localhost:8080/pipelines/object_detection/1",vasRequestOption)
+   //resp, err = grequests.Post("http://localhost:8080/pipelines/object_detection/1",vasRequestOption)
+   resp, err := grequests.Post(endPointStr,vasRequestOption)
    if err != nil {
      log.Fatalln ("Post Failed with: ", err)
+     return err
    }
    log.Println("Pipeline Instance Created:", resp.String())
    var instance = strings.TrimSpace(resp.String())
-   
+
    // Interface: GET /pipelines/{name}/{version}/{instance_id}/status for Return pipeline instance status.
    // Loop status query until instantance completed.
-   var statusResp VASInstanceStatus    
+   var statusResp VASInstanceStatus
    for {
-     resp, err := grequests.Get("http://localhost:8080/pipelines/object_detection/1/"+instance+"/status", nil)
+     //resp, err := grequests.Get("http://localhost:8080/pipelines/object_detection/1/"+instance+"/status", nil)
+     resp, err := grequests.Get(endPointStr+"/"+instance+"/status", nil)
      if err != nil {
        log.Fatalln ("Get Failed with: ", err)
+       return err
      }
      log.Println(resp.String())
      resp.JSON(&statusResp)
@@ -79,8 +75,8 @@ func main () {
         break
      }
      time.Sleep(time.Duration(10)*time.Second)
-     
-   }
 
+   }
+   return nil
 
 }
---
+++ applications/vas-sample-app/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2020 Intel Corporation
+
+go run main.go application.go eaa_interface.go
+fg
