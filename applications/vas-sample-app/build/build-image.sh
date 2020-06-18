33b86ffb8315712904719c81890eae01c50ff646
xiaopeng,tong <xiaopeng.tong@intel.com>
2020-06-18 17:47:10 +0800

    add helmchart

--- applications/vas-sample-app/build/build-image.sh
+++ applications/vas-sample-app/build/build-image.sh
@@ -3,7 +3,7 @@
 # PDX-License-Identifier: Apache-2.0
 # Copyright (c) 2020 Intel Corporation
 
-sudo docker build \
+sudo docker build -f ./build/Dockerfile \
     --build-arg http_proxy=$http_proxy \
     --build-arg https_proxy=$https_proxy \
     --build-arg no_proxy=$no_proxy \
