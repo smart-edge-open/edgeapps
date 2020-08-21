6603f9ea6364033e1b4da7f661213c0e4d49929c
zhaoyuex <zhaoyuex.cui@intel.com>
2020-08-21 15:57:25 +0800

    Optimize the OpenNESS with K8s Benchmark APP for CPU and HDDL-R mode and Test Automation for openvino benchmark

--- applications/openvino/benchmark/do_benchmark.sh
+++ applications/openvino/benchmark/do_benchmark.sh
@@ -5,7 +5,7 @@
 
 source /opt/intel/openvino/bin/setupvars.sh
 cd /opt/intel/openvino/deployment_tools/demo
-./demo_squeezenet_download_convert_run.sh -d HDDL
+./demo_squeezenet_download_convert_run.sh -d ${TARGET_DEVICE}
 
-/root/inference_engine_samples_build/intel64/Release/benchmark_app -i /opt/intel/openvino/deployment_tools/demo/car.png -m /root/openvino_models/ir/public/squeezenet1.1/FP16/squeezenet1.1.xml -d HDDL -nireq 32 -niter 9999999 -api async
+/root/inference_engine_samples_build/intel64/Release/benchmark_app -i /opt/intel/openvino/deployment_tools/demo/car.png -m /root/openvino_models/ir/public/squeezenet1.1/FP16/squeezenet1.1.xml -d ${TARGET_DEVICE} -nireq ${NIREQ} -niter ${NITER} -api async
 
