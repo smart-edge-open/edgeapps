265ec68a8b49f87d89da0e7c8db0b67331aca6af
zhaoyuex <zhaoyuex.cui@intel.com>
2020-09-17 16:01:37 +0800

    add double quote for environment variables in do_benchmark.sh file

--- applications/openvino/benchmark/do_benchmark.sh
+++ applications/openvino/benchmark/do_benchmark.sh
@@ -5,7 +5,7 @@
 
 source /opt/intel/openvino/bin/setupvars.sh
 cd /opt/intel/openvino/deployment_tools/demo
-./demo_squeezenet_download_convert_run.sh -d ${TARGET_DEVICE}
+./demo_squeezenet_download_convert_run.sh -d "${TARGET_DEVICE}"
 
 /root/inference_engine_samples_build/intel64/Release/benchmark_app -i "${IMAGE}" -m "${MODEL}" -d "${TARGET_DEVICE}" -nireq "${NIREQ}" -niter "${NITER}" -api "${API}" -b "${BATCH_SIZE}"
 
