69208a2dcd05c81f2952e9144dec732539b7cc20
Joanna Kossakowska <joanna.kossakowska@intel.com>
2020-11-26 11:05:56 +0000

    Adding files to be used for setting SDEWAN cnf E2E scenario on one single node cluster

---
+++ network-functions/sdewan_cnf/e2e-scenarios/one-single-node-cluster/iperf_files/iperf_img/iperf_client.sh
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2019-2020 Intel Corporation
+#!/bin/bash
+
+# Modify the iperf-server IP as per the setup
+server_ip=192.168.2.2
+
+# Modify interface name (net0) if needed
+client_ip=`ifconfig net0 | grep inet | awk '{print $2}'`
+
+duration=300
+streams=1
+pkt_size=128
+
+# Start Iperf client to send TCP stream towards Iperf server
+echo "Iperf client started sending TCP packet stream"
+echo "server_ip=$server_ip, client_ip=$client_ip, duration=$duration, streams=$streams, packet_size=$pkt_size"
+
+iperf3 -c $server_ip -B $client_ip -J -b 0 -t $duration -P $streams -M $pkt_size -l 1M
+
+echo "Stopped Iperf TCP streaming"
---
+++ network-functions/sdewan_cnf/e2e-scenarios/one-single-node-cluster/iperf_files/iperf_server.sh
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2020 Intel Corporation
+#!/bin/bash
+
+# Provide the interface for the server to start on
+interface="eno1"
+
+if [ -z "$interface" ]
+then
+  echo -e "\tThe interface for the Iperf server to listen on is not set"
+  exit 1
+fi
+
+server_ip=$(ifconfig $interface | grep inet | awk '{print $2}')
+
+echo "iperf3 server is starting at IP: $server_ip"
+
+#iperf3 -s "$server_ip" -i 60
+
+echo "iperf3 server is stopped"
