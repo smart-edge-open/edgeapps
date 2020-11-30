76c93d37e55b4e5a69c8f0e7163c3ac8576b3e31
jkossak <48013160+jkossak@users.noreply.github.com>
2020-11-30 15:22:47 +0000

    Adding files to be used for setting SDEWAN cnf E2E scenario on one si… (#160)
    * Adding files to be used for setting SDEWAN cnf E2E scenario on one single node cluster
    
    * Fixes to linter errors
    
    * Fixed linter errors
    
    * Fixed linter errors and using env variables used for network configuration
    
    * Fixed formatting
    
    * Added missing shebang to node_config.sh
    
    * Added README file
    
    * Added script creating pod and network specs, change Dockerfile base to Alpine
    
    * Fixed linter error

---
+++ network-functions/sdewan_cnf/e2e-scenarios/one-single-node-cluster/iperf_files/create_iperf_cli_pod.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2020 Intel Corporation
+
+IPERF_POD_OVN_INTF=${IPERF_POD_OVN_INTF}
+IPERF_POD_OVN_IP=${IPERF_POD_OVN_IP}
+
+cat > iperf_cli_pod.yaml << EOF
+---
+
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: iperf-client
+  labels:
+    app: iperf-client
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: iperf-client
+  template:
+    metadata:
+      labels:
+        app: iperf-client
+      annotations:
+        k8s.v1.cni.cncf.io/networks: '[{ "name": "ovn-networkobj"}]'
+        k8s.plugin.opnfv.org/nfn-network: '{ "type": "ovn4nfv", "interface": [{"name": "ovn-network", "interface": "$IPERF_POD_OVN_INTF", "ipAddress": "$IPERF_POD_OVN_IP"}]}'
+    spec:
+      containers:
+      - name: iperf-client
+        image: iperf-client:1.0
+        imagePullPolicy: Never
+        ports:
+        - containerPort: 80
+        - containerPort: 443
+
+EOF
