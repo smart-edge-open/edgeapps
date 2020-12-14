9caaa5883a13b7988f37d6aa0815aef2e7840971
Oek User <dev@openness.com>
2020-12-14 15:27:26 +0000

    fix parser error

--- network-functions/sdewan_cnf/e2e-scenarios/three-single-node-clusters/edge2/set_tunnel_rules.sh
+++ network-functions/sdewan_cnf/e2e-scenarios/three-single-node-clusters/edge2/set_tunnel_rules.sh
@@ -16,19 +16,18 @@ kubectl exec -it -n "${NS:-default}" "$CNFPOD" -- iptables -t nat -A POSTROUTING
 # These command just to set ovelay route SNAT rule.
 NET3_IFC=net3
 # find the overlay IP allocated by tunnel.
-IPNET=$(kubectl exec -it -n "${NS:-default}" "$CNFPOD" -- ip a | grep "${O_UE2_IP%.*}" | awk '{match($0, /.+inet\s([^ ]*)/, a);print a[1];exit}'`)
+IPNET=$(kubectl exec -it -n "${NS:-default}" "$CNFPOD" -- ip a | grep "${O_UE2_IP%.*}" | awk '{match($0, /.+inet\s([^ ]*)/, a);print a[1];exit}')
 # SNAT, UE2 IP of p1 interface map to overlay IP of EDGE2 net3 interface
 TO_SOURCE=${IPNET%%/*}    # TO_SOURCE=$O_UE2_IP
 kubectl exec -it -n "${NS:-default}" "$CNFPOD" -- iptables -t nat -A POSTROUTING -o "$NET2_IFC" -s "$UE2_P1" -j SNAT --to-source "$TO_SOURCE"
 # If bidirectional need, then need DNAT
 kubectl exec -it -n "${NS:-default}" "$CNFPOD" -- iptables -t nat -I PREROUTING -d "${IPNET%%/*}" -j DNAT --to-destination "$UE2_P1"
 
+# watch the status by this command
+# watch -n 1 kubectl exec -it  "$CNFPOD" -- iptables -t nat -L -n --line -v
 
 # route to NET3
 NET=${NET3?"Error: not set"}
 VIA=${HUB_CNF_NET4_IFIP?"Error: not set"}
 INTERFACE=net3
 kubectl exec -it "$CNFPOD" -- ip r a "$NET" via "$VIA" dev "$INTERFACE"
-
-# watch the status by this command
-# watch -n 1 kubectl exec -it  "$CNFPOD" -- iptables -t nat -L -n --line -v
