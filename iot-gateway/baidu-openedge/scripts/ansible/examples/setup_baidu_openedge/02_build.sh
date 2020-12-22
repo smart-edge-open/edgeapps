2717d95dce0e566f50aa98eba7dc72aa4de7ee8e
Jiang <zhenggang1x.jiang@intel.com>
2020-12-22 15:16:30 +0800

    fix shellcheck errors(OP_6286)

--- iot-gateway/baidu-openedge/scripts/ansible/examples/setup_baidu_openedge/02_build.sh
+++ iot-gateway/baidu-openedge/scripts/ansible/examples/setup_baidu_openedge/02_build.sh
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019 Intel Corporation
 
+# shellcheck disable=SC1091
 source ../../common/scripts/ansible-precheck.sh
 source ../../common/vars/task_log_file.sh
 ansible-playbook ./tasks/build_main.yml -i ../../common/vars/hosts --connection=local
