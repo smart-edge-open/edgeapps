eb0db2eaadd7e46a0a2651a5d1f229dadb799af6
Jiang Zhenggang <zhenggang1x.jiang@intel.com>
2020-12-29 05:51:10 -0500

    update consumer image

--- applications/openvino/consumer/start.sh
+++ applications/openvino/consumer/start.sh
@@ -3,9 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019 Intel Corporation
 
-# shellcheck disable=SC1091
 source /opt/intel/openvino/bin/setupvars.sh
 echo "192.168.1.10 analytics.openness" >> /etc/hosts
 ./fwd.sh &
-go run main.go openvino.go eaa_interface.go
+./main
 fg
