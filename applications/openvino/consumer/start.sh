f4bd7fcbaad6e7cf02bbcc8e5d70c631f042000c
Jiang Zhenggang <zhenggang1x.jiang@intel.com>
2021-01-14 13:40:20 +0800

    fix shellcheck

--- applications/openvino/consumer/start.sh
+++ applications/openvino/consumer/start.sh
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019 Intel Corporation
 
-source $OPENVINO_ROOT/bin/setupvars.sh
+# shellcheck disable=SC1091
+source "$OPENVINO_ROOT"/bin/setupvars.sh
 echo "192.168.1.10 analytics.openness" >> /etc/hosts
 nginx -g "daemon on;"
 ./main
