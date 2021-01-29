6373a9a7fa178c43a66acca18761bfb535f2a7c9
Jiang Zhenggang <zhenggang1x.jiang@intel.com>
2021-01-29 17:40:09 +0800

    update the start.sh for client simulator

--- applications/openvino/clientsim/start.sh
+++ applications/openvino/clientsim/start.sh
@@ -4,8 +4,9 @@
 # Copyright (c) 2019 Intel Corporation
 
 trap "exit" SIGINT SIGTERM
-set -m
 ./tx_video.sh &
-sleep 3
-taskset -c 1 ffplay -i rtmp://openvino.openness:5000/live/out.flv
-fg
+
+while true
+do
+  taskset -c 1 ffplay -i rtmp://openvino.openness:5000/live/out.flv -an
+done
