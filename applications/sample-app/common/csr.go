74e1534bf83b9cff3de79c88ef482730398ec7bf
Tomasz Wesolowski <tomaszx.wesolowski@intel.com>
2021-03-16 08:51:00 +0100

    Add TLS minversion variable

--- applications/sample-app/common/csr.go
+++ applications/sample-app/common/csr.go
@@ -35,6 +35,7 @@ func CreateEncryptedClient() (*http.Client, error) {
 			TLSClientConfig: &tls.Config{RootCAs: certPool,
 				Certificates: []tls.Certificate{cert},
 				ServerName:   Cfg.EaaCommonName,
+				MinVersion:   tls.VersionTLS12,
 			},
 		}}
 
