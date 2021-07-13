d2f40d1cebb789c0cb506e993c6123a0da3bb1ac
umanathlanka <umanath.lanka@exium.net>
2021-07-13 15:26:42 +0530

    commented variables that are not used in application - Initial commit of xe-cnf

--- applications/xe-cnf/config.sh
+++ applications/xe-cnf/config.sh
@@ -4,14 +4,14 @@
 # Copyright (c) 2021 Exium Inc.
 
 
-nwu_ip='xx.xx.xx.xx'
-nwu_mask='xx'
-nwu_pci_addr='0000:xx:00.0'
-nwu_gw_ip='xx.xx.xx.xx'
-n6_ip='xx.xx.xx.xx'
-n6_mask='xx'
-n6_pci_addr='0000:xx:00.0'
-n6_gw_ip='xx.xx.xx.xx'
+#nwu_ip='xx.xx.xx.xx'
+#nwu_mask='xx'
+#nwu_pci_addr='0000:xx:00.0'
+#nwu_gw_ip='xx.xx.xx.xx'
+#n6_ip='xx.xx.xx.xx'
+#n6_mask='xx'
+#n6_pci_addr='0000:xx:00.0'
+#n6_gw_ip='xx.xx.xx.xx'
 exedge_sctp_ip='xx.xx.xx.xx'
 
 # For connectivity to 5GC , reach out to Exium support team
